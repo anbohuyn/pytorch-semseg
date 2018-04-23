f2da07310b7b380f6855716a7d1f1b29f140818b
abhyudai <abhyudai007@gmail.com>
2018-04-22 23:23:06 -0400

    Add colours for segmentation

--- ptsemseg/loader/kitti_loader.py
+++ ptsemseg/loader/kitti_loader.py
@@ -9,6 +9,31 @@ from ptsemseg.utils import recursive_glob
 from ptsemseg.augmentations import *
 
 class kittiLoader(data.Dataset):
+
+    colors = [#[  0,   0,   0],
+              [128,  64, 128],
+              [244,  35, 232],
+              [ 70,  70,  70],
+              [102, 102, 156],
+              [190, 153, 153],
+              [153, 153, 153],
+              [250, 170,  30],
+              [220, 220,   0],
+              [107, 142,  35],
+              [152, 251, 152],
+              [ 0, 130, 180],
+              [220,  20,  60],
+              [255,   0,   0],
+              [  0,   0, 142],
+              [  0,   0,  70],
+              [  0,  60, 100],
+              [  0,  80, 100],
+              [  0,   0, 230],
+              [119,  11,  32]]
+
+    label_colours = dict(zip(range(19), colors))
+
+
     def __init__(self, root, split="train", is_transform=False, 
                 img_size=None, augmentations=None, img_norm=True):
        """__init__
